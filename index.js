2b36897cd0c5a1d02dbe7903dba87d5e75f44a83
ChiwKA <hoangquan96@gmail.com>
2025-07-19 14:57:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns')
 const app = express();
 
 // Basic Configuration
@@ -10,19 +11,28 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/shorturl', function(req, res) {
-  res.json({ greeting: req.body });
-});
+// app.get('/api/shorturl', function(req, res) {
+//   res.json({ greeting: req.body });
+//   dns.lookup('store.steampowered.com', (err, address, family) => {
+//     if (err) return console.error(err);
+//     console.log('address:', address)
+//     console.log('family:', family)
+//   })
+// });
 
 app.post('/api/shorturl', function(req, res) {
-  res.json({
-    original_url: req.path
-  })
+  const data = req.body
+  console.log('url:', data.url)
+  res.send('Form submitted')
 })
 
 app.listen(port, function() {
